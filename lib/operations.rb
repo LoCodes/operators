6e0e75f52a76885034251aedbe1cb0c139938f38
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-12 13:41:40 -0800

    commit tree 534e1da0be2b76f8ad7ae16f864a9ad38394bda4

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,23 @@
+#def unsafe?(speed)
+#    speed ? speed > 60 ||  speed < 40 : 40 < speed > 60
+#end
+
+
 def unsafe?(speed)
+    if speed > 60
+        true
+    elsif speed < 40 
+        true
+    else speed < 40 || speed > 60
+        false
+    end 
+end 
 
-end
 
-
-
+#Tenary Operator
 def not_safe?(speed)
-	
-end
-	
+    speed ? speed > 60 || speed < 40 : 40 < speed > 60
+end 
+
 
 
